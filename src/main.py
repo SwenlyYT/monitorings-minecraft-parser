93869fd3f825396932d5c24dc2ab907a69475bc6
SwenlyYT <93844393+SwenlyYT@users.noreply.github.com>
2022-05-06 20:57:51 +0300

    Update main.py

--- src/main.py
+++ src/main.py
@@ -12,9 +12,9 @@ soup = BS(r.content, 'html.parser')
 _need_ver = input('Какая версия? ')
 if _need_ver == '' or _need_ver == '\n': need_ver = 'all'
 else: need_ver = _need_ver
-_need_no_online = input('Нужно ли убирать серваки c 0 онлайном? ')
-if _need_no_online.lower() == 'да' or _need_no_online == '': need_no_online = 'да'
-else: need_no_online = 'нет'
+#_need_no_online = input('Нужно ли убирать серваки c 0 онлайном? ')
+#if _need_no_online.lower() == 'да' or _need_no_online == '': need_no_online = 'да'
+#else: need_no_online = 'нет'
 _aternos = input('Убирать атерносы? ')
 if _aternos.lower() == 'да' or _aternos == '': aternos = 'да'
 else: aternos = _aternos
@@ -33,7 +33,7 @@ for i in mm_servers:
     except: continue
     if aternos == 'да':
         if 'aternos' in ip.text: continue
-        if ip.text.startswith('185'): continue
+        if ip.text.startswith('185') or ip.text.startswith('193'): continue
     version = i.find('td', class_='ver')
     if need_ver != 'all':
         if str(version.text) != need_ver: continue
@@ -45,8 +45,8 @@ for i in mm_servers:
         if int(list[0]) < int(min_online):
             continue
     cur_online = f'{list[0]} {list[1]} {list[2]}'
-    if need_no_online == 'да':
-        if list[0] == '0': continue
+#    if need_no_online == 'да':
+#        if list[0] == '0': continue
     print('Сервер')
     print('-' * len(f' Название сервера: "{name.text}"'))
     print(f' Название сервера: {name.text}')
@@ -70,7 +70,7 @@ for i in mr_servers:
     if ip.text in mm_servers: continue
     if aternos == 'да':
         if 'aternos' in ip.text: continue
-        if ip.text.startswith('185'): continue
+        if ip.text.startswith('185') or ip.text.startswith('193'): continue
     version = i.find('i', class_='fal fa-check-circle').find_parent()
     if need_ver != 'all':
         if str(version.text) != need_ver: continue
@@ -78,9 +78,9 @@ for i in mr_servers:
     if min_online != 'all':
         if int(online) < int(min_online):
             continue
-    if need_no_online == 'да':
+#    if need_no_online == 'да':
         if str(online) == '0': continue
-    if 'Offline' in name.text or 'License only' in name.text: continue
+#    if 'Offline' in name.text or 'License only' in name.text: continue
     print('Сервер')
     print('-' * len(f'Название сервера: "{name.text}"'))
     print(f' Название сервера: "{name.text}"')
@@ -102,7 +102,7 @@ for i in ml_servers:
     if ip.text in mm_servers or ip.text in mr_servers: continue
     if aternos == 'да':
         if 'aternos' in ip.text: continue
-        if ip.text.startswith('185'): continue
+        if ip.text.startswith('185') or ip.text.startswith('193'): continue
     version = i.find('i', class_='far fa-check-circle').find_parent().text.split('версия')[0]
     if need_ver != 'all':
         if str(version) != need_ver: continue
@@ -110,8 +110,8 @@ for i in ml_servers:
     if min_online != 'all':
         if int(online) < int(min_online):
             continue
-    if need_no_online == 'да':
-        if str(online) == '0': continue
+#    if need_no_online == 'да':
+#        if str(online) == '0': continue
     print('Сервер')
     print('-' * len(f'Название сервера: "{name.text}"'))
     print(f' Название сервера: "{name.text}"')
